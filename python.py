5dfa19fc8eabf7eb6897a2329c789f3cec5c4c9e
sughoshbhat2211 <sughoshbhat979@gmail.com>
2026-05-06 13:22:43 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,17 +1,59 @@
+!pip install qrcode[pil]
+
 import qrcode
 from IPython.display import display
+import ipywidgets as widgets
+from IPython.display import clear_output
 
-qr = qrcode.QRCode(
-    version=4,
-    box_size=10,
-    border=5
+# Input box
+text_box = widgets.Text(
+    value='https://www.google.com',
+    description='Link:',
+    layout=widgets.Layout(width='500px')
 )
 
-qr.add_data("https://www.google.com")
-qr.make(fit=True)
+# Button
+button = widgets.Button(
+    description='Generate QR',
+    button_style='success'
+)
 
-img = qr.make_image(fill_color="black", back_color="white")
+# Output area
+output = widgets.Output()
 
-img.save("website_qr.png")
+# Function
+def generate_qr(b):
 
-display(img)
+    with output:
+
+        clear_output()
+
+        # Get input value
+        data = text_box.value
+
+        # Create QR
+        qr = qrcode.QRCode(
+            version=4,
+            box_size=10,
+            border=5
+        )
+
+        qr.add_data(data)
+        qr.make(fit=True)
+
+        img = qr.make_image(
+            fill_color="black",
+            back_color="white"
+        )
+
+        img.save("website_qr.png")
+
+        display(img)
+
+# Button click event
+button.on_click(generate_qr)
+
+# Show UI
+display(text_box)
+display(button)
+display(output)
